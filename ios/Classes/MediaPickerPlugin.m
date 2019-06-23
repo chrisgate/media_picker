ce1458372d70e1e24ce277538b4b9b6794b53f51
puming <pmchongqing@163.com>
2019-06-23 22:05:48 +0800

    init media_picker

---
+++ ios/Classes/MediaPickerPlugin.m
@@ -0,0 +1,20 @@
+#import "MediaPickerPlugin.h"
+
+@implementation MediaPickerPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"media_picker"
+            binaryMessenger:[registrar messenger]];
+  MediaPickerPlugin* instance = [[MediaPickerPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
